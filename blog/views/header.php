653faf0558cbae632112e9eb2aab64b01a9f5cb9
malyga <40186295+malyga@users.noreply.github.com>
2019-02-28 20:50:37 +0100

    css limpio

--- blog/views/header.php
+++ blog/views/header.php
@@ -11,9 +11,9 @@
 			<a href="<?php echo RUTA; ?>">LOGO</a>
 		</div>
 		<div class="busqueda">
-			<form name="busqueda" class="busqueda" action="<?php echo RUTA; ?>/buscar.php" method="get">
+			<form name="busqueda" action="<?php echo RUTA; ?>/buscar.php" method="get">
 				<input type="text" name="busqueda" placeholder="Buscar">
 				<button type="submit" value="Buscar">Buscar</button>
 			</form>
-		</div>		
+		</div>
 	</div>
